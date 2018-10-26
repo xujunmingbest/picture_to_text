c918ffc26449aa0cf7d65099772d09c540cc44e3
xjm <925271237@qq.com>
2018-10-26 11:22:45 +0800

    first commit

---
+++ ptt/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ptt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MODI.Document doc = new MODI.Document();
+
+            doc.Create("1.png");
+
+            MODI.Image image;
+
+            MODI.Layout layout;
+
+            doc.OCR(MODI.MiLANGUAGES.miLANG_CHINESE_SIMPLIFIED, true, true);  // 识别简体中文
+            string sb;
+            for (int i = 0; i < doc.Images.Count; i++)
+
+            {
+
+                image = (MODI.Image)doc.Images[i];
+
+                layout = image.Layout;
+
+                Console.WriteLine(layout.Text);
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
